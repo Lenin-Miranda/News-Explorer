cadfec73182e402932ae1b6577e8016e86db5702
LeninMiranda <lenin9073@gmail.com>
2025-07-17 11:23:33 -0700

    API/update

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -1,15 +1,3 @@
 import { checkResponse } from "./Api";
 const apiKey = "494e42497aaa4cd8ba25c47c7bdcb23f";
 const API_URL = "https://newsapi.org/v2/everything";
-
-export const fetchNews = async (query = "bitcoin") => {
-  try {
-    const res = await fetch(
-      `${API_URL}?q=${encodeURIComponent(query)}&apiKey=${apiKey}&pageSize=100`
-    );
-    return await checkResponse(res);
-  } catch (err) {
-    console.error("Error al obtener noticias:", err);
-    throw err;
-  }
-};
